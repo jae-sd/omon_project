fb5b2642eabc69a13eb9c53e866a5f6b69c18dff
Emmanuel Ajike <emmanuelajike2000@gmail.com>
2023-03-09 20:39:26 +0100

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,12 +1,12 @@
 const mongoose = require("mongoose")
 
 let mongodbAtlas = `mongodb+srv://jae321:${process.env.DB_PASSWORD}@cluster0.gwvtatj.mongodb.net/omon`;
-let localConnection = "mongodb://localhost:27017/omon";
+//let localConnection = "mongodb://localhost:27017/omon";
 
 const connectDb = async (cb) => {
 
     mongoose.set('strictQuery', true);
-    mongoose.connect(mongodbAtlas && localConnection,
+    mongoose.connect(mongodbAtlas,
         {
             useNewUrlParser: true,
             useUnifiedTopology: true
